83633af41da2114f41fb5c272ed43008ff652e24
wellington <wellington@programer.com.br>
2023-06-29 20:24:16 -0300

    monday sdk test ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import mondaySdk from "monday-sdk-js";
+import { Button } from "monday-ui-react-core";
 import './App.css';
+const monday= mondaySdk();
 
 function App() {
+
+  // const [countries, setCountries] = useState([])
+  //
+  // useEffect(() => {
+  //     monday.api(`query {
+  //       boards (ids: 4505502032) {
+  //         items {
+  //           id, name
+  //         }
+  //       }
+  //     }`).then(({data}) => setCountries(data as any))
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header>
+          <Button>Hello</Button>
+        {/*{(countries.length !==  0)&&<div style={{color:`white`}}>{JSON.stringify(countries)}</div>}*/}
+        {/*{(countries.length === 0)&&<div style={{color:`white`}}>Loading...</div>}*/}
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        {/*<p>*/}
+        {/*  Edit <code>src/App.tsx</code> and save to reload.*/}
+        {/*</p>*/}
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
     </div>
   );
